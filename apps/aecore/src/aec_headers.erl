bd684a51ad8e85909f6dab28bbc550457b0d84d0
sennui <michal@sennui.com>
2018-05-25 15:54:57 +0200

    output format

--- apps/aecore/src/aec_headers.erl
+++ apps/aecore/src/aec_headers.erl
@@ -218,7 +218,7 @@ validate_pow({#header{nonce        = Nonce,
     %% Zero nonce and pow_evidence before hashing, as this is how the mined block
     %% got hashed.
     Header1 = Header#header{nonce = 0, pow_evidence = no_value},
-    HeaderBinary = hash_header(Header1),
+    {ok, HeaderBinary} = hash_header(Header1),
     case aec_pow_cuckoo:verify(HeaderBinary, Nonce, Evd, Target) of
         true ->
             ok;
